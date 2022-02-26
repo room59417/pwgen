d87c422716ce776ee1b58c0361d5efedff0c345b
room591 <75628385+room59417@users.noreply.github.com>
2022-02-27 00:48:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-#Password Generator Project
+#Password Generator demo
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '-']
 
-print("Welcome to Alex's Password Generator! It is the most secure password you will ever have!")
+print("Welcome to Alex's Password Generator! The most secure password you will ever have!")
 nr_letters = int(input("How many letters you want in your password?\n")) 
 nr_symbols = int(input(f"How many symbols?\n"))
 nr_numbers = int(input(f"How many numbers?\n"))
